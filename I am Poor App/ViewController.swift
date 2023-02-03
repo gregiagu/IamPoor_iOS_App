a39aaadde474a05df89f254dcd6fee8418092ac1
Gustavo Gregianin <gustavo.gregianin@usp.br>
2023-02-02 21:41:47 -0300

    Add content

---
+++ I am Poor App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor App
+//
+//  Created by Gustavo Gregianin on 02/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
